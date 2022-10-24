3a0c4547529ad60c2299fd6d0c4d4efd6c8a5c59
gabriel <gabrielcmoris@gmail.com>
2022-10-24 16:08:38 +0200

    compiler

--- 2beyond-fundamentals/compiler/app.js
+++ 2beyond-fundamentals/compiler/app.js
@@ -1,7 +1,21 @@
+// WITH COMPILER
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
-    }
-  }
-}).mount('#app')
+      message: "Hello world!",
+    };
+  },
+  template: `{{message}}`,
+}).mount("#app");
+
+//WITHOUT COMPILER
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+  render() {
+    return Vue.h("h1", this.message);
+  },
+}).mount("#app2");
