76db5e162d78f0c11ed866c0836d6c685db99bb1
gabriel <gabrielcmoris@gmail.com>
2022-10-25 16:24:31 +0200

    slots

--- components/src/App.vue
+++ components/src/App.vue
@@ -2,20 +2,50 @@
   <h3>Hey!</h3>
   <HelloWorld :age="age" />
   <User :age="age" @age-change="updateAge" :ageChangeFn="updateAgeCB" />
+  <AppForm>
+    <template v-slot:help>
+      <p>{{ help }}</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+    <p>Dummy Text</p>
+  </AppForm>
+  <AppForm>
+    <template v-slot:help>
+      <p>This is a second Form.</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </AppForm>
+  <AppForm></AppForm>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
 import User from "@/components/User.vue";
+import AppForm from "@/components/AppForm.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
     User,
+    AppForm,
   },
   data() {
     return {
       age: 20,
+      help: "This is just a help text for the form",
     };
   },
   methods: {
---
+++ components/src/components/AppForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <div class="help">
+      <slot name="help">Default message: Form message</slot>
+    </div>
+    <div class="fields">
+      <slot name="fields">Default message: Form fields</slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons">Default message: Form buttons</slot>
+    </div>
+    <!-- This will make accept every extra html that I send -->
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default { name: "AppForm" };
+</script>
