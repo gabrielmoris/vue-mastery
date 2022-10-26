6c3f5ac0eed6cd0f3838b81f2837965dfc368713
gabriel <gabrielcmoris@gmail.com>
2022-10-26 08:39:13 +0200

    dynamic components

---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Home Page</div>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log(
+      "Wrapped with <keep-alive></keep-alive> this component wont be unmounted"
+    );
+  },
+  activated() {
+    console.log(
+      "Home has been actiated in life cycle instead mounted() because is wrapped with <keep-alive></keep-alive>"
+    );
+  },
+  deactivated() {
+    console.log(
+      "Home has been deactiated in life cycle instead unmounted() because is wrapped with <keep-alive></keep-alive>"
+    );
+  },
+};
+</script>
+
+<style scoped></style>
