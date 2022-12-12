4403d6ee42827552372cc315be9b31270aaea44c
gabriel <gabrielcmoris@gmail.com>
2022-12-12 18:11:00 +0100

    mixins

---
+++ mixins/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="scrollPosition">{{ offset }}</div>
+  </div>
+</template>
+
+<script>
+import mixin from "@/mixin";
+export default {
+  name: "App",
+  mixins: [mixin],
+  // If I create data that overlaps the mixin, the mixin has piority
+  data() {
+    return {
+      offset: 1,
+    };
+  },
+  mounted() {
+    alert("It also works puttint mounted() in both places");
+  },
+};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 3000px;
+  position: relative;
+}
+
+.scrollPosition {
+  position: fixed;
+  left: 10px;
+  right: 10px;
+}
+</style>
