044094c03532074126cf958569e83a23ef3e2198
gabriel <gabrielcmoris@gmail.com>
2022-12-13 18:59:55 +0100

    data and refs

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ num }}</p>
+    <button @click.prevent="increment">increment</button>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "App",
+  setup() {
+    // We need to make the number reactive, that is why we use ref
+    let num = ref(0);
+    function increment() {
+      // to change the value of the ref, we acces by variable.value
+      num.value++;
+    }
+    return { num, increment };
+  },
 };
 </script>
