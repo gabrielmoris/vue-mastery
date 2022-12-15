1083dd66387a980b1f5210220e9e60ed30ef5d20
gabriel <gabrielcmoris@gmail.com>
2022-12-15 10:15:23 +0100

    composition + pinia

--- composition-router-hooks/src/views/About.vue
+++ composition-router-hooks/src/views/About.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="about">
     <h1>{{ $route.params.member }}</h1>
+    <p>{{ store.counter }}</p>
+    <p>{{ store.doubleCount }}</p>
+    <button type="button" @click.prevent="store.increment">Increment</button>
   </div>
 </template>
 
 <script>
 import { useRoute, useRouter } from "vue-router";
+import { useCounterStore } from "../stores/counter";
 export default {
   setup() {
     const route = useRoute(); // Equivalent to: this.$route
     const router = useRouter(); // this.$router
+    const store = useCounterStore();
+
     console.log(route.params.member);
     router.push({
       hash: "#test",
     });
+    return { store };
   },
 };
 </script>
