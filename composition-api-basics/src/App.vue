5ce6e0082ca0786187c97126a541f883b0ac773d
gabriel <gabrielcmoris@gmail.com>
2022-12-16 10:38:47 +0100

    script setup

--- composition-api-basics/src/App.vue
+++ composition-api-basics/src/App.vue
@@ -13,62 +13,66 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { ref, reactive, toRefs, onBeforeMount, onMounted } from "vue";
 import AppAlert from "@/components/Alert.vue";
 import { useNumber } from "@/hooks/number";
 import { useSentence } from "@/hooks/sentence";
 
-export default {
-  name: "App",
-  components: { AppAlert },
-  //after the name we put all the logic in the setup()
-  setup() {
-    // The setup is create before "this" exists
-    //To use template Refs I do this and the variable must have the same name as the ref in template
-    // The rest of it is handled by vue, But i Can accsss only after the component is mounted()
-    const btn = ref(null);
-    //lifecycle functions
-    onBeforeMount(() => {
-      console.log("onBeforeMount!");
-    });
-    onMounted(() => {
-      console.log("onMounted!");
-      btn.value.addEventListener("click", () => {
-        alert("Clicked!");
-      });
-    });
+// With setup in script I dont need this
+// export default {
+// name: "App",
+// components: { AppAlert },
+//after the name we put all the logic in the setup()
+// setup() {
+// The setup is create before "this" exists
+//To use template Refs I do this and the variable must have the same name as the ref in template
+// The rest of it is handled by vue, But i Can accsss only after the component is mounted()
+const btn = ref(null);
+//lifecycle functions
+onBeforeMount(() => {
+  console.log("onBeforeMount!");
+});
+onMounted(() => {
+  console.log("onMounted!");
+  btn.value.addEventListener("click", () => {
+    alert("Clicked!");
+  });
+});
 
-    //To make it reactive I dont use ref, i use reactive instead
-    const user = reactive({
-      name: "John",
-      age: 20,
-    });
-    const user2 = reactive({ name: "gabriel", age: 20 });
+//To make it reactive I dont use ref, i use reactive instead
+const user = reactive({
+  name: "John",
+  age: 20,
+});
+const user2 = reactive({ name: "gabriel", age: 20 });
 
-    setTimeout(() => {
-      user.name = "CENA";
-      user2.name = "Saludos!";
-    }, 3000);
+setTimeout(() => {
+  user.name = "CENA";
+  user2.name = "Saludos!";
+}, 3000);
 
-    //Here I call my hooks
-    // I still have to return them from Setup()
-    const { num, double, increment } = useNumber();
-    const { sentence, reversed, mixed, num: sentenceNum } = useSentence();
+//Here I call my hooks
+// I still have to return them from Setup()
+const { num, double, increment } = useNumber();
+const { sentence, reversed, mixed, num: sentenceNum } = useSentence();
 
-    // Every variable or function used in the template must be returned
-    return {
-      num,
-      increment,
-      user,
-      ...toRefs(user2),
-      sentence,
-      reversed,
-      mixed,
-      double,
-      btn,
-      sentenceNum,
-    };
-  },
-};
+// Every variable or function used in the template must be returned unles I use the script return
+// return {
+//   num,
+//   increment,
+//   user,
+//   ...toRefs(user2),
+//   sentence,
+//   reversed,
+//   mixed,
+//   double,
+//   btn,
+//   sentenceNum,
+// };
+// },
+// };
+
+/// I need to make a little change when Iexport the object
+const { name } = toRefs(user2);
 </script>
