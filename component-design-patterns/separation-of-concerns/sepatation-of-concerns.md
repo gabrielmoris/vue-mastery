90647c2bb6b1e5d1a82fac90d14a6058ccf8056b
gabriel <gabrielcmoris@gmail.com>
2023-01-02 11:37:39 +0100

    separation of concerns

---
+++ component-design-patterns/separation-of-concerns/sepatation-of-concerns.md
@@ -0,0 +1,11 @@
+## There are 2 tyupes of logic in an application:
+
+# UI Logic
+
+The code inside the Template block of a Component (v-for, v-if, etc...)
+
+# Business Logic
+
+The code that handles the data in the application
+
+##### Separation of concerns is the idea of separating different types of logic
---
+++ component-design-patterns/separation-of-concerns/src/components/EmailInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    type="email"
+    class="form-control"
+    placeholder="E-mail"
+    :value="modelValue"
+    @input="update($event)"
+    :class="{
+      'is-valid': validateEmail(modelValue),
+      'is-invalid': !validateEmail(modelValue),
+    }"
+  />
+</template>
+
+<script>
+import validateEmail from "../validateEmail(separation-of-concerns)";
+export default {
+  name: "EmailInput",
+  props: ["modelValue"],
+  methods: {
+    update($event) {
+      // I am updating the prop "modelvalue"
+      this.$emit("update:modelValue", $event.target.value);
+    },
+    validateEmail,
+  },
+};
+</script>
