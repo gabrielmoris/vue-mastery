8e2b59831375d9fc9406bd8b5bea81e619d0a89e
gabriel <gabrielcmoris@gmail.com>
2023-01-02 13:41:55 +0100

    encapsulate scrolling

---
+++ component-design-patterns/encapsulate-scrolling/src/App.vue
@@ -0,0 +1,184 @@
+<template>
+  <div id="app">
+    <div class="mt-2 ml-2">
+      <button type="button" class="btn btn-primary" @click="open = true">
+        Open
+      </button>
+    </div>
+    <app-modal :show="open" @hide="open = false" />
+    <p class="p-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+      qui, laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus,
+      in dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Ad qui, laborum voluptatibus accusamus, in dicta, ipsam neque
+      exercitationem reprehenderit optio minima culpa vel blanditiis accusantium
+      provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus, in
+      dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+      qui, laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Ad qui, laborum voluptatibus
+      accusamus, in dicta, ipsam neque exercitationem reprehenderit optio minima
+      culpa vel blanditiis accusantium provident ipsa. Incidunt, molestias
+      iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad qui,
+      laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus,
+      in dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.
+    </p>
+    <p class="p-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+      qui, laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus,
+      in dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Ad qui, laborum voluptatibus accusamus, in dicta, ipsam neque
+      exercitationem reprehenderit optio minima culpa vel blanditiis accusantium
+      provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus, in
+      dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+      qui, laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Ad qui, laborum voluptatibus
+      accusamus, in dicta, ipsam neque exercitationem reprehenderit optio minima
+      culpa vel blanditiis accusantium provident ipsa. Incidunt, molestias
+      iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad qui,
+      laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus,
+      in dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.
+    </p>
+    <p class="p-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+      qui, laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus,
+      in dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Ad qui, laborum voluptatibus accusamus, in dicta, ipsam neque
+      exercitationem reprehenderit optio minima culpa vel blanditiis accusantium
+      provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus, in
+      dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+      qui, laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Ad qui, laborum voluptatibus
+      accusamus, in dicta, ipsam neque exercitationem reprehenderit optio minima
+      culpa vel blanditiis accusantium provident ipsa. Incidunt, molestias
+      iste.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad qui,
+      laborum voluptatibus accusamus, in dicta, ipsam neque exercitationem
+      reprehenderit optio minima culpa vel blanditiis accusantium provident
+      ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Ad qui, laborum voluptatibus accusamus, in dicta, ipsam
+      neque exercitationem reprehenderit optio minima culpa vel blanditiis
+      accusantium provident ipsa. Incidunt, molestias iste.Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Ad qui, laborum voluptatibus accusamus,
+      in dicta, ipsam neque exercitationem reprehenderit optio minima culpa vel
+      blanditiis accusantium provident ipsa. Incidunt, molestias iste.Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Ad qui, laborum
+      voluptatibus accusamus, in dicta, ipsam neque exercitationem reprehenderit
+      optio minima culpa vel blanditiis accusantium provident ipsa. Incidunt,
+      molestias iste.
+    </p>
+  </div>
+</template>
+
+<script>
+import AppModal from "@/components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppModal,
+  },
+  data() {
+    return {
+      open: true,
+    };
+  },
+};
+</script>
+
+<style src="bootstrap/dist/css/bootstrap.css"></style>
